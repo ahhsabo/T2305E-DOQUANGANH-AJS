4075a411320b84eb104b705d53ffba9906e79c85
ahhsabo <anhdk2000@gmail.com>
2023-09-07 20:04:35 +0700

    1

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,84 +1,16 @@
 import { HttpClient } from "@angular/common/http";
 import { Component } from "@angular/core";
 @Component({
-    selector:'app-home',
+    selector: 'app-home',
     templateUrl: "./home.component.html",
     styleUrls: ['./home.component.css']
 })
-export class HomeComponent{
-    ToDo = [
-        {
-            "name":"Task 1",
-            "content":"content 1"
-        },
-        {
-            "name":"Task 3",
-            "content":"content 2"
-        },
-        {
-            "name":"task 10",
-            "content":"content 3"
-        },
-        {
-            "name":"task 20",
-            "content":"content 4"
-        }
-    ]
-    InProgress = [
-        {
-            "name":"task 5",
-            "content":"content 1"
-        },
-        {
-            "name":"task 6",
-            "content":"content "
-        },
-        {
-            "name":"task 7",
-            "content":"content 3"
-        },
-        {
-            "name":"task 8",
-            "content":"content 4"
-        }
-    ]
+export class HomeComponent {
+    ToDo = [{ "name": "task 1", "content": "Analyze the new requirements gathered from the customer" }, { "name": "task 2", "content": "Improve application performance" }, { "name": "task 3", "content": "Arrange a web meeting with the customer to get new requirements" }, { "name": "task 4", "content": "Fix the issues reported in the IE browser" }]
+    InProgress = [{ "name": "task 5", "content": "Fix the issues reported by the customer" }, { "name": "task 6", "content": "Fix the issues reported in Safari browser" }, { "name": "task 7", "content": "Test the application in the IE browser" }, { "name": "task 8", "content": "Analyze SQL server 2008 connection" }]
 
-    InReview = [
-        {
-            "name":"task 5",
-            "content":"content 1"
-        },
-        {
-            "name":"task 6",
-            "content":"content "
-        },
-        {
-            "name":"task 7",
-            "content":"content 3"
-        },
-        {
-            "name":"task 8",
-            "content":"content 4"
-        }
-    ]
+    InReview = [{ "name": "task 1", "content": "Analyze the new requirements gathered from the customer" }, { "name": "task 2", "content": "Improve application performance" }, { "name": "task 3", "content": "Arrange a web meeting with the customer to get new requirements" }, { "name": "task 4", "content": "Fix the issues reported in the IE browser" }]
+    Done = [{ "name": "task 5", "content": "Fix the issues reported by the customer" }, { "name": "task 6", "content": "Fix the issues reported in Safari browser" }, { "name": "task 7", "content": "Test the application in the IE browser" }, { "name": "task 8", "content": "Analyze SQL server 2008 connection" }]
 
-    Done = [
-        {
-            "name":"task 5",
-            "content":"content 1"
-        },
-        {
-            "name":"task 6",
-            "content":"content "
-        },
-        {
-            "name":"task 7",
-            "content":"content 3"
-        },
-        {
-            "name":"task 8",
-            "content":"content 4"
-        }
-    ]
 
 }
