24f58c12ce2dc7bd97d38f8f4043e97ed9ba6675
DoQuangAnh <anhdk2000@gmail.com>
2023-09-08 08:36:30 +0700

    Update home.component.ts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,11 +6,76 @@ import { Component } from "@angular/core";
     styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-    ToDo = [{ "name": "Task 1", "content": "Analyze the new requirements gathered from the customer" }, { "name": "Task 3", "content": "Improve application performance" }, { "name": "Task 3", "content": "Arrange a web meeting with the customer to get new requirements" }, { "name": "Task 4", "content": "Fix the issues reported in the IE browser" }]
-    InProgress = [{ "name": "Task 5", "content": "Fix the issues reported by the customer" }, { "name": "Task 6", "content": "Fix the issues reported in Safari browser" }, { "name": "Task 7", "content": "Test the application in the IE browser" }, { "name": "Task 8", "content": "Analyze SQL server 2008 connection" }]
+ ToDo = [
+        {
+            "name":"Task 1",
+            "content":"Analyze the new requirements gathered from the customer."
+        },
+        {
+            "name":"Task 3",
+            "content":"Arrange a web meeting with the customer to get new requirements."
+        },
+        {
+            "name":"Task 10",
+            "content":"Show the retrieved data from the server in grid control."
+        }
+    ]
 
-    InReview = [{ "name": "Task 1", "content": "Analyze the new requirements gathered from the customer" }, { "name": "Task 2", "content": "Improve application performance" }, { "name": "Task 3", "content": "Arrange a web meeting with the customer to get new requirements" }, { "name": "Task 4", "content": "Fix the issues reported in the IE browser" }]
-    Done = [{ "name": "Task 5", "content": "Fix the issues reported by the customer" }, { "name": "Task 6", "content": "Fix the issues reported in Safari browser" }, { "name": "Task 7", "content": "Test the application in the IE browser" }, { "name": "Task 8", "content": "Analyze SQL server 2008 connection" }]
+    InProgress = [
+        {
+            "name":"Task 2",
+            "content":"Improve application performance."
+        },
+        {
+            "name":"Task 4",
+            "content":"Fix the issue reported in the IE browser."
+        },
+        {
+            "name":"Task 11",
+            "content":"Fix cannot open user's default database SQL error."
+        },
+        {
+            "name":"Task 21",
+            "content":"Improve the performance of the editing functionality."
+        }
+    ]
 
+    InReview = [
+        {
+            "name":"Task 5",
+            "content":"Fix the issue reported by the customer."
+        },
+        {
+            "name":"Task 7",
+            "content":"Fix the issue reported in Safari browser."
+        },
+        {
+            "name":"Task 12",
+            "content":"Fix the issue reported in data binding."
+        },
+        {
+            "name":"Task 19",
+            "content":"Test editing functionality."
+        }
+    ]
+
+    Done = [
+        {
+            "name":"Task 8",
+            "content":"Test the application in the IE browser."
+        },
+        {
+            "name":"Task 13",
+            "content":"Analyze SQL server 2008 connection."
+        },
+        {
+            "name":"Task 15",
+            "content":"Analyze grid control."
+        },
+        {
+            "name":"Task 16",
+            "content":"Stored procedure for intial data binding of the grid."
+        }
+    ]
 
 }
